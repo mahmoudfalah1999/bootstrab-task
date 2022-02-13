fc367b7261641beed8342113ac0b216b348d4189
mahmoud alkhatib <mahmood.alkateeb999@gmail.com>
2022-02-13 23:42:52 +0200

    first commit

---
+++ first.php
@@ -0,0 +1,25 @@
+<!doctype html>
+<html>
+<BODY>
+<h1>first PHP task  by mahmoud alkhatib </h1>
+<?php
+// 
+echo "hello PHP" ;
+echo "<br>";
+$x=7;
+$y=9;
+$flout=5.23;
+echo"varible 1 is = $x <br> " . "varible 2 is = $y <br>";
+print " this is my first paragraph "."<br>";
+var_dump($x)."<br>";
+
+var_dump($flout)."<br>";
+
+echo"this is randomly number" .rand()."<br>";
+echo " the most round number to 5.23 is = " .round ($flout);
+?>
+</body>
+</html>
+   
+
+
