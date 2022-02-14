97c1421ab5375a56da42876b0aa4f12b077f3f87
mahmoud alkhatib <mahmood.alkateeb999@gmail.com>
2022-02-14 19:55:54 +0200

    task2firstcomment

---
+++ task 2.php
@@ -0,0 +1,25 @@
+
+<?php
+// 1 page 44 
+$n = "do your task"."<br>";
+echo $n;
+//2 page 44
+echo strlen("what is your name?")."<br>";
+//3 page 44
+echo str_word_count("my name is mahmoud")."<br>";
+//4 page 44
+echo strrev ("my name is mahmoud")."<br>";
+//5 page 45""
+echo strpos("welcome to my task page","task")."<br>";
+//6 page 45
+echo str_replace ("farah","sarah","this is farah page")."<br>";
+//7 page 45
+define("name","farah",);
+$name="sara";
+echo name."<br>";
+
+
+
+?>
+
+   
